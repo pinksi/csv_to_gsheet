b6a8881db2d7ecf3d89273653fb8be19c2efbddb
pinkyy <pinky.sitikhu@docsumo.com>
2020-07-02 22:16:54 +0545

    converts into class-based approach

--- main.py
+++ main.py
@@ -6,44 +6,50 @@ import os
 from loguru import logger
 from datetime import datetime
 
-# use creds to create a client to interact with the Google Drive API
-scope = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive",
-]
-creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
-client = gspread.authorize(creds)
 
-# export single csvfile to a spreadsheet
-def export_single_csv(s_id, csvfile):
-    try:
-        sh = client.open_by_key(s_id)
-        file_name = csvfile.split("/")[-1].split(".")[0]
-        f_name = file_name + "_" + str(datetime.date(datetime.now()))
-        sheetName = sh.add_worksheet(title=f_name, rows="1000", cols="50")
-        sh.values_update(
-            sheetName.title,
-            params={"valueInputOption": "USER_ENTERED"},
-            body={"values": list(csv.reader(open(csvfile)))},
+class ExportToSheet:
+    def __init__(self, spreadsheet_id):
+        # use creds to create a client to interact with the Google Drive API
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/drive",
+        ]
+        creds = ServiceAccountCredentials.from_json_keyfile_name(
+            "client_secret.json", scope
         )
-    except Exception as e:
-        logger.error(f"Error: Exception {e} occurred!")
-        pass
+        self.client = gspread.authorize(creds)
+        self.spreadsheet_id = spreadsheet_id
 
+    # export single csvfile to a spreadsheet
+    def export_single_csv(self, csvfile):
+        try:
+            logger.info(f"Exporting single file to first sheet of google spreadsheet!!")
+            sh = self.client.open_by_key(self.spreadsheet_id)
+            file_name = csvfile.split("/")[-1].split(".")[0]
+            f_name = file_name + "_" + str(datetime.date(datetime.now()))
+            sheetName = sh.add_worksheet(title=f_name, rows="1000", cols="50")
+            sh.values_update(
+                sheetName.title,
+                params={"valueInputOption": "USER_ENTERED"},
+                body={"values": list(csv.reader(open(csvfile)))},
+            )
+        except Exception as e:
+            logger.error(f"Error: Exception {e} occurred!")
+            pass
 
-# export multiple csvfiles to different worksheets
-def export_multiple_csv(s_id, files):
-    for file in files:
-        export_single_csv(s_id, file)
+    # export multiple csvfiles to different worksheets
+    def export_multiple_csv(self, files):
+        logger.info(f"Exporting all files to different worksheet of same spreadsheet!!")
+        for file in files:
+            self.export_single_csv(file)
 
 
 def main():
     spreadsheet_id = "1TvZHFhFDA9-l0DogBm825rbN8ZBlOealqhFTxvD3cAk"
     files = glob.glob(f"{os.getcwd()}/csvfiles/*.csv")
-    # logger.info(f"Exporting first file to first sheet of google spreadsheet!!")
-    # export_single_csv(spreadsheet_id, files[0])
-    logger.info(f"Exporting all files to different worksheet of same spreadsheet!!")
-    export_multiple_csv(spreadsheet_id, files)
+    obj = ExportToSheet(spreadsheet_id)
+    obj.export_single_csv(files[0])
+    obj.export_multiple_csv(files)
     logger.success("DONE")
 
 
